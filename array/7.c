09c46789a3d6fb37da3c6f1cf88c7f93b9f22aa8
Shahidul <theshahidkhan.2004@gmail.com>
2025-04-15 23:44:31 +0600

    fixed error

--- array/7.c
+++ array/7.c
@@ -4,12 +4,13 @@ int main (void) {
     int n;
     scanf("%d", &n);
     int arr[n];
-    int rev_arr[n];
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    for (int i = 0, j = n - 1; i < n; i++, j--) {
-        rev_arr[j] = arr[i];
+    for (int i = 0, j = n - 1; i < j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    for (int i = 0; i < n; i++) printf("%d ", rev_arr[i]);
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
 }
--- temp.c
+++ temp.c
@@ -105,4 +105,10 @@ int main (void) {
 
     //     printf("\n");
     // }
+
+    // int n = 5, factorial = 1;
+    // for (int i = n; i > 1; i--) factorial *= i;
+    // printf("%d", factorial);
+
+    
 }
