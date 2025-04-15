d178764065a02992ee626cbf01cbed321b34dfc9
Shahidul <theshahidkhan.2004@gmail.com>
2025-04-16 00:19:42 +0600

    fixed error

--- array/13.c
+++ array/13.c
@@ -3,13 +3,13 @@
 int main (void) {
     int n, pos;
     scanf("%d", &n);
-    int arr[n + 1];
+    int arr[n];
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     printf("position: ");
     scanf("%d", &pos);
-    for (int i = pos; i < n; i++) arr[i] = arr[i + 1];
+    for (int i = pos; i < n - 1; i++) arr[i] = arr[i + 1];
     for (int i = 0; i < n - 1; i++) {
         printf("%d ", arr[i]);
     }
