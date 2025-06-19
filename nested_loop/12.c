931bca33a1ca22757abdbd19e5aa4aad8b94afa2
Shahidul <theshahidkhan.2004@gmail.com>
2025-06-20 01:44:06 +0600

    refactored a few problems

--- nested_loop/12.c
+++ nested_loop/12.c
@@ -3,23 +3,15 @@
 int main (void) {
     int n;
     scanf("%d", &n);
-    n = (n / 2) + 1;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            (j <= n - i) ? printf("_") : printf("*");
-        }
-        for (int k = 1; k < i; k++) {
-            printf("*");
-        }
+    int m = n / 2 + 1;
+    for (int i = 1, p = 1; i <= m; i++, p += 2) {
+        for (int j = 1; j <= m - i; j ++) printf("_");
+        for (int k = 1; k <= p; k++) printf("*");
         printf("\n");
     }
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j <= n; j++) {
-            (j <= i) ? printf("_") : printf("*");
-        }
-        for (int k = 1; k < n - i; k++) {
-            printf("*");
-        }
+    for (int i = 1, q = n - 2; i < m; i++, q -= 2) {
+        for (int j = 1; j <= i; j++) printf("_");
+        for (int k = 1; k <= q; k++) printf("*");
         printf("\n");
     }
 }
--- nested_loop/13.c
+++ nested_loop/13.c
@@ -4,16 +4,13 @@ int main (void) {
     int n;
     scanf("%d", &n);
     int col = 2 * n - 1;
-    for (int i = 1; i <= n; i++) {
-        int k;
-        if (i < n) k = i; else k = i - 1;
-        for (int j = 1; j <= col; j++) {
-            if (j <= i) printf("%d", j);
-            else if (j <= col - i) printf(" ");
-            else {
-                printf("%d", k);
-                k--;
-            }
+    for (int i = 1, p = col - 2; i <= n; i++, p -= 2) {
+        for (int j = 1; j <= i; j++) printf("%d", j);
+        for (int k = 1; k <= p; k++) printf("_");
+        if (i < n) {
+            for (int l = i; l >= 1; l--) printf("%d", l);
+        } else {
+            for (int l = i - 1; l >= 1; l--) printf("%d", l);
         }
         printf("\n");
     }
