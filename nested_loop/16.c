39dd1b1e17de5b5b1f1149b724a220b98d94cf2b
Shahidul <theshahidkhan.2004@gmail.com>
2025-06-20 12:50:54 +0600

    refactored a few problems

--- nested_loop/16.c
+++ nested_loop/16.c
@@ -3,9 +3,9 @@
 int main (void) {
     int n;
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1, k = n; i <= n; i++, k--) {
         for (int j = 1; j <= n; j++) {
-            (i == j || j == n - i + 1) ? printf("*") : printf ("_");
+            (i == j || j == k) ? printf("*") : printf ("_");
         }
         printf("\n");
     }
