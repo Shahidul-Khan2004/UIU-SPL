12da5a6c8bd5b60d1a83ce3d89f5e058f4188830
Shahidul <theshahidkhan.2004@gmail.com>
2025-06-25 21:35:41 +0600

    addded a recursion problem

---
+++ recursion/7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int palindrome (char str[], int start, int end) {
+    if (start == end) return 1;
+    if (str[start] != str[end]) return 0;
+    else if (start - end == 1) return 1;
+    palindrome(str, start + 1, end - 1);
+}
+
+int main (void) {
+    char s[64];
+    scanf("%[^\n]", s);
+    int len = 0;
+    while (s[len] != 0) len++;
+    if(palindrome(s, 0, len - 1)) printf("Palindrome");
+    else printf("Oh hell nah!");
+}
