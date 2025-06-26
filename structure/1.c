2cb90dc5d78986060e6c107e8e8409ade4fd372b
Shahidul <theshahidkhan.2004@gmail.com>
2025-06-26 07:44:43 +0600

    added struct problems

---
+++ structure/1.c
@@ -0,0 +1,5 @@
+struct student {
+    char name[32];
+    char id[16];
+    float cgpa;
+};
---
+++ structure/2.c
@@ -0,0 +1,7 @@
+struct student {
+    char name[32];
+    char id[16];
+    float cgpa;
+};
+
+struct student s1 = {"Unknown", "N/A", 0.0};
