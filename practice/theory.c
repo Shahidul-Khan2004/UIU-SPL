43fe7b8b22f2816ef6fd58a36a9db64f9c7b2b49
Shahidul <theshahidkhan.2004@gmail.com>
2025-07-09 12:46:08 +0600

    updated theory problem

--- practice/theory.c
+++ practice/theory.c
@@ -1,15 +1,19 @@
 #include <stdio.h> 
 #include <string.h> 
  
-int fun(int *a, int *b){ 
-if (*a > 1)  
-return *a + *b; 
-else  
-return *b - *a; 
-} 
-int main(void) { 
-int x = -10;  
-int y = 20;  
-printf("The result is %d", 
-fun(&x, &y)); 
+void f(char *s1, char *s2, int n, int *a) {     
+int i;      
+for(i=0; i<n; i++) {          
+if(s1[i] > s2[i]) *a = *a+1;            
+else if(s1[i]<s2[i]) *(s1+i)=*(s2+i); 
+else if(s1[i] == s2[i]) *a = *a-1; 
+}  
+}  
+void main() {      
+int a, b;      
+char s1[] = "worldcupBangladesh";      
+char s2[] = "BanglaWash";      
+a = 0;      
+f(s1,s2,13,&a);        
+printf("%d\n s1=%s , s2=%s\n",a,s1,s2);  
 } 
