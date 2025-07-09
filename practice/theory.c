910eb78405fa538b555556673b8b7f72042fcf14
Shahidul <theshahidkhan.2004@gmail.com>
2025-07-09 08:28:28 +0600

    changed the theory problem

--- practice/theory.c
+++ practice/theory.c
@@ -1,16 +1,15 @@
 #include <stdio.h> 
-void modifyArray(int arr[], int 
-size){ 
-for (int i = 0; i < size; i++){ 
-arr[i] *= 2; 
+void go(int num){ 
+printf("How are you?\n"); 
+if(num==0){ 
+return; 
 } 
+num /= 10; 
+go(num); 
+printf("I am fine\n"); 
 } 
-void main() { 
-int numbers[] = {1, 2, 3, 4, 5}; 
-int *ptr = numbers; 
-modifyArray(ptr, 5); 
-for(int i = 0; i < 5; i++){ 
-*(ptr+i) = *(ptr+i) + i; 
-printf("%d ", *(ptr+i)); 
-} 
-} 
+int main(void){ 
+int num = 45633; 
+go(num); 
+return 0; 
+}
