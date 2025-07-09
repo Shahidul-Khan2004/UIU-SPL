a22f7098ae79ed9016711c5f17f7f7963a46a55a
Shahidul <theshahidkhan.2004@gmail.com>
2025-07-09 08:45:40 +0600

    completed file

---
+++ practice/file.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void) {
+    FILE *fp;
+    fp = fopen("in.txt", "r");
+    if (fp == 0) printf("Couldn't open in.txt");
+    int max = -9999999;
+    int num;
+    while (fscanf(fp, "%d", &num) == 1) {
+        if (num > max) max = num;
+    }
+    // while (fscanf(fp, "%d", &num) != EOF) {
+    //     if (num > max) max = num;
+    // }
+    fclose(fp);
+    FILE *fp1;
+    fp1 = fopen("out.txt", "w");
+    if (fp1 == 0) printf("couldn't open out.txt");
+    fprintf(fp1, "%d", max);
+    fclose(fp1);
+}
