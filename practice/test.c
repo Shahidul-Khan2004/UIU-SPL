b0f61b5f88b7fcdea59a09e0fb7a2a077b492e66
Shahidul <theshahidkhan.2004@gmail.com>
2025-07-09 16:47:36 +0600

    tested a new one

--- practice/test.c
+++ practice/test.c
@@ -1,10 +1,15 @@
-#include <stdio.h>
-
-int main (void) {
-    char str[128];
-    int n;
-    scanf("%[^\n]", str);
-    scanf("%d", &n);
-    for (int i = n - 1; str[i] != 0; i += n) str[i] = 'Z';
-    printf("%s", str);
-}
+#include <stdio.h> 
+#include <string.h> 
+int main() { 
+char str1[50] = "Programming"; 
+char str2[50] = "Languages"; 
+int len = strlen(str1); 
+for (int i = 0; i < strlen(str2); i++) 
+str1[len - i - 1] = str2[i]; 
+strncat(str1, str2, 3); 
+for (int j = 0; j < 5; j++) 
+str1[j] = str2[strlen(str2) - j - 1]; 
+printf("str1: %s\n", str1); 
+printf("str2: %s\n", str2); 
+return 0; 
+} 
