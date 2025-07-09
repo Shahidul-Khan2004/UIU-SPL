058092806de49d75fb65edc1b774c27cc1670429
Shahidul <theshahidkhan.2004@gmail.com>
2025-07-09 13:31:16 +0600

    added theory

--- practice/theory.c
+++ practice/theory.c
@@ -1,19 +1,15 @@
 #include <stdio.h> 
-#include <string.h> 
- 
-void f(char *s1, char *s2, int n, int *a) {     
-int i;      
-for(i=0; i<n; i++) {          
-if(s1[i] > s2[i]) *a = *a+1;            
-else if(s1[i]<s2[i]) *(s1+i)=*(s2+i); 
-else if(s1[i] == s2[i]) *a = *a-1; 
-}  
-}  
-void main() {      
-int a, b;      
-char s1[] = "worldcupBangladesh";      
-char s2[] = "BanglaWash";      
-a = 0;      
-f(s1,s2,13,&a);        
-printf("%d\n s1=%s , s2=%s\n",a,s1,s2);  
-} 
+
+void swap (int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main (void) {
+    int A[5];
+    int *aPtr = A;
+    for (int i = 0; i < 5; i++) scanf("%d", aPtr + i);
+    swap(&A[2], &A[3]);
+    for (int i = 0; i < 5; i++) printf("%d ", *(aPtr + i));
+}
