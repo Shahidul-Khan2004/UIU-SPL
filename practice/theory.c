4085a05cf5ce20bc5fb19f42be44440e91ccc4bd
Shahidul <theshahidkhan.2004@gmail.com>
2025-07-10 08:28:37 +0600

    tested another theory

--- practice/theory.c
+++ practice/theory.c
@@ -1,17 +1,13 @@
 #include <stdio.h> 
-void inc(int *ap, int dummy){ 
-    for(int i=0; i<dummy; i++){ 
-        *ap = *ap + 1; 
-        ap = ap + 2; 
-    } 
-    dummy = 100; 
+int power_of_2(int n) { 
+   printf("Inside power_of_2(%d)\n", 
+n); 
+       if(n == 1) return 1; 
+       if(n % 2 != 0) return 0; 
+       return power_of_2(n / 2); 
 } 
-int main() { 
-    int a[] = {1,2,3,4,5,6,7,8}; 
-    int dummy = 3; 
-    inc(&a[2], dummy); 
-    for(int i=0; i<8; i++)   
-           printf("%d ",a[i]); 
-    printf("\nDummy=%d\n", dummy); 
+int main(void){ 
+    int num = 16; 
+    printf("%d",power_of_2(num)); 
     return 0; 
-}
+} 
