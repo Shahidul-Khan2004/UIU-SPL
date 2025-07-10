eebd1f518c856af2a1b3c521b8e1581e067381e2
Shahidul <theshahidkhan.2004@gmail.com>
2025-07-10 08:23:59 +0600

    tested another theory

--- practice/theory.c
+++ practice/theory.c
@@ -1,15 +1,19 @@
-#include <stdio.h> 
-
-void swap (int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int main (void) {
-    int A[5];
-    int *aPtr = A;
-    for (int i = 0; i < 5; i++) scanf("%d", aPtr + i);
-    swap(&A[2], &A[3]);
-    for (int i = 0; i < 5; i++) printf("%d ", *(aPtr + i));
+ 
+#include<stdio.h> 
+int x = 2, y = 3; 
+int fun1(int n){ 
+    return n%11; 
+} 
+void fun2(int arr[], int n){ 
+    for(int i = 0; i<n; i++){ 
+        x = fun1(x) + fun1(y); 
+        arr[i] = arr[i] + x; 
+        y = fun1(y) + fun1(x); 
+    } 
+} 
+int main(){ 
+    int a[] = {2, 3, 5, 7, 11}; 
+    fun2(a, 5); 
+    for(int i = 0; i<5; i++) 
+        printf("%d ", a[i]); 
 }
