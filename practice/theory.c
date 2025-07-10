2099762c25eb465e3dabc3c829433d48bffe422f
Shahidul <theshahidkhan.2004@gmail.com>
2025-07-10 08:24:06 +0600

    tested another theory

--- practice/theory.c
+++ practice/theory.c
@@ -1,19 +1,17 @@
- 
-#include<stdio.h> 
-int x = 2, y = 3; 
-int fun1(int n){ 
-    return n%11; 
-} 
-void fun2(int arr[], int n){ 
-    for(int i = 0; i<n; i++){ 
-        x = fun1(x) + fun1(y); 
-        arr[i] = arr[i] + x; 
-        y = fun1(y) + fun1(x); 
+#include <stdio.h> 
+void inc(int *ap, int dummy){ 
+    for(int i=0; i<dummy; i++){ 
+        *ap = *ap + 1; 
+        ap = ap + 2; 
     } 
+    dummy = 100; 
 } 
-int main(){ 
-    int a[] = {2, 3, 5, 7, 11}; 
-    fun2(a, 5); 
-    for(int i = 0; i<5; i++) 
-        printf("%d ", a[i]); 
+int main() { 
+    int a[] = {1,2,3,4,5,6,7,8}; 
+    int dummy = 3; 
+    inc(&a[2], dummy); 
+    for(int i=0; i<8; i++)   
+           printf("%d ",a[i]); 
+    printf("\nDummy=%d\n", dummy); 
+    return 0; 
 }
